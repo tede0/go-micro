8f171ef36724f564417170dd768cbdedf6b4a624
Maksym Lyskov <admin@Maksyms-MBP.lan>
2024-01-12 15:08:24 +0000

    added log testing button to FE

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -28,46 +28,59 @@ const Home = () => {
     setRecieved(result.response)
   }
 
+  const TestLog = async (name: string, data: string) => {
+    const result: PageData = await CallLog(name, data)
+    setSent(result.request)
+    setRecieved(result.response)
+  }
+
   return (
     <>
-      <div className='container w-3/4 mx-auto'>
-        <h1 className='text-center font-bold text-2xl p-4'>
+      <div className="container w-3/4 mx-auto">
+        <h1 className="text-center font-bold text-2xl p-4">
           Test Microservices
         </h1>
         <hr />
 
-        <div className='mt-5 flex gap-2'>
+        <div className="mt-5 flex gap-2">
           <button
             onClick={() => TestBroker()}
-            className='bg-blue-500 hover:bg-blue-700 rounded-md text-white p-3'
+            className="bg-blue-500 hover:bg-blue-700 rounded-md text-white p-3"
           >
             Test Broker
           </button>
 
           <button
             onClick={() => TestAuth('admin@example.com', 'verysecret')}
-            className='bg-blue-500 hover:bg-blue-700 rounded-md text-white p-3'
+            className="bg-blue-500 hover:bg-blue-700 rounded-md text-white p-3"
           >
             Test Auth
           </button>
+
+          <button
+            onClick={() => TestLog('event', 'Test Log')}
+            className="bg-blue-500 hover:bg-blue-700 rounded-md text-white p-3"
+          >
+            Test Log
+          </button>
         </div>
 
-        <div className='mt-10 p-5 w-full border border-grey'>
-          <div className='font-bold'>Output:</div>
+        <div className="mt-10 p-5 w-full border border-grey">
+          <div className="font-bold">Output:</div>
           <div>{recieved?.message}</div>
         </div>
 
-        <div className='mt-10 flex flex-row gap-1'>
-          <div className='basis-1/2'>
+        <div className="mt-10 flex flex-row gap-1">
+          <div className="basis-1/2">
             <h3>Sent</h3>
-            <pre className='border min-h-20 p-3'>
+            <pre className="border min-h-20 p-3">
               {JSON.stringify(sent, undefined, 4)}
             </pre>
           </div>
 
-          <div className='basis-1/2'>
+          <div className="basis-1/2">
             <h3>Recieved</h3>
-            <pre className='border min-h-20 p-3'>
+            <pre className="border min-h-20 p-3">
               {JSON.stringify(recieved, undefined, 4)}
             </pre>
           </div>
@@ -75,6 +88,52 @@ const Home = () => {
       </div>
     </>
   )
+}
+
+async function CallLog(name: string, data: string) {
+  const payload = {
+    action: 'Log',
+    log: {
+      name: name,
+      data: data,
+    },
+  }
+
+  try {
+    const response: AxiosResponse = await axios({
+      method: 'POST',
+      url: 'http://localhost:8080/handle',
+      headers: { 'Content-Type': 'application/json' },
+      data: payload,
+    })
+
+    return {
+      request: payload,
+      response: {
+        error: false,
+        message: response.data.message,
+        data: response.data.data,
+      },
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        request: payload,
+        response: {
+          error: true,
+          message: err.message,
+        },
+      }
+    }
+  }
+
+  return {
+    request: payload,
+    response: {
+      error: true,
+      message: 'Unknown error',
+    },
+  }
 }
 
 async function CallAuth(email: string, password: string): Promise<PageData> {
