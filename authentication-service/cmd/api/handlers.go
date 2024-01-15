0302b1e64d1fd081fd14763bfaacdec42f893cde
Maksym Lyskov <admin@Maksyms-MacBook-Pro.local>
2024-01-15 10:33:24 +0000

    finished logger

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -8,7 +10,7 @@ import (
 
 func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	var requestPayload struct {
-		Email string `json:"email"`
+		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
 
@@ -16,7 +18,7 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		app.errorJSON(w, err, http.StatusBadRequest)
-		return  
+		return
 	}
 
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
@@ -28,16 +30,50 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	valid, err := user.PasswordMatches(requestPayload.Password)
 
-	if err != nil  || !valid {
+	if err != nil || !valid {
 		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	payload := jsonResponse {
-		Error: false,
+	// err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	// if err != nil {
+	// 	app.errorJSON(w, err)
+	// 	return
+	// }
+
+	payload := jsonResponse{
+		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
-		Data: user,
+		Data:    user,
 	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logRequest(name string, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := http.Client{}
+
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
